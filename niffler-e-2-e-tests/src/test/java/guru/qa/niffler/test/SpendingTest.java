34eb2a78b9ec1f0c759038c07f344f9834e0572c
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2024-04-23 22:22:33 +0300

    add category extension

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/SpendingTest.java
@@ -2,37 +2,46 @@ package guru.qa.niffler.test;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
-import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.annotation.GenerateCategory;
+import guru.qa.niffler.jupiter.annotation.GenerateSpend;
+import guru.qa.niffler.jupiter.extension.CategoryExtension;
 import guru.qa.niffler.jupiter.extension.SpendExtension;
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import guru.qa.niffler.pages.LoginPage;
 import guru.qa.niffler.pages.MainPage;
 import guru.qa.niffler.pages.WelcomePage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
+@ExtendWith(CategoryExtension.class)
 @ExtendWith(SpendExtension.class)
 public class SpendingTest {
     private final WelcomePage welcomePage = new WelcomePage();
-    private final LoginPage loginPage = new LoginPage();
     private final MainPage mainPage = new MainPage();
+    private final String USERNAME = "oleg";
+    private final String PASSWORD = "1234";
+    private final String CATEGORY = "Обучение";
 
 
     static {
         Configuration.browserSize = "1920x1080";
+        Configuration.browserCapabilities = new ChromeOptions()
+                .addArguments("--incognito");
     }
 
     @BeforeEach
     void doLogin() {
         // createSpend
         Selenide.open("http://127.0.0.1:3000/");
-        welcomePage.openLoginPage();
-        loginPage.signIn("oleg", "1234");
+        welcomePage
+                .openLoginPage()
+                .signIn(USERNAME, PASSWORD);
     }
 
     @Test
@@ -41,12 +50,17 @@ public class SpendingTest {
         $("a[href*='redirect']").should(visible);
     }
 
-    @Spend(
-            username = "oleg",
+
+    @GenerateCategory(
+            username = USERNAME,
+            category = CATEGORY
+    )
+    @GenerateSpend(
+            username = USERNAME,
             description = "QA.GURU Advanced 5",
             amount = 65000.00,
             currency = CurrencyValues.RUB,
-            category = "Обучение"
+            category = CATEGORY
     )
     @Test
     void spendingShouldBeDeletedAfterTableAction(SpendJson spendJson) {
