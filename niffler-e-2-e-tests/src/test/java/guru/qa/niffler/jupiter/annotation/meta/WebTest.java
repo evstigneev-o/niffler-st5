07905aff60e22db998f87cbbe215549dd23fa056
Oleg Evstigneev <o.evstigneev@citydrive.ru>
2024-04-30 12:11:07 +0300

    code from lesson

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/meta/WebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/annotation/meta/WebTest.java
@@ -3,7 +3,6 @@ package guru.qa.niffler.jupiter.annotation.meta;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
 import guru.qa.niffler.jupiter.extension.CategoryExtension;
 import guru.qa.niffler.jupiter.extension.SpendExtension;
-import guru.qa.niffler.jupiter.extension.UserQueueExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.lang.annotation.ElementType;
