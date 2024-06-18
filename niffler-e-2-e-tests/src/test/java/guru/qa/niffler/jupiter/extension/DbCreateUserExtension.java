74b37b491b61e2ce93f73bab6eb9200b5735f34e
Oleg Evstigneev <evstigneeff08@gmail.com>
2024-06-18 16:26:50 +0300

    add json ftl and allure json attachment

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/DbCreateUserExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/DbCreateUserExtension.java
@@ -3,15 +3,21 @@ package guru.qa.niffler.jupiter.extension;
 import guru.qa.niffler.data.entity.UserAuthEntity;
 import guru.qa.niffler.data.entity.UserEntity;
 import guru.qa.niffler.data.repository.UserRepository;
+import guru.qa.niffler.data.repository.logging.AllureJsonAppender;
 import guru.qa.niffler.model.UserJson;
 
 public class DbCreateUserExtension extends AbstractCreateUserExtension {
     private final UserRepository userRepository = UserRepository.getInstance();
+    private final AllureJsonAppender allureJsonAppender = new AllureJsonAppender();
 
     @Override
     protected UserJson createUser(UserJson user) {
-        userRepository.createUserInAuth(UserAuthEntity.fromJson(user));
-        userRepository.createUserInUserdata(UserEntity.fromJson(user));
+        UserAuthEntity userAuthEntity = UserAuthEntity.fromJson(user);
+        UserEntity userEntity = UserEntity.fromJson(user);
+        allureJsonAppender.logJson("userAuthEntity", userAuthEntity);
+        allureJsonAppender.logJson("userEntity", userEntity);
+        userRepository.createUserInAuth(userAuthEntity);
+        userRepository.createUserInUserdata(userEntity);
         return user;
     }
 }
